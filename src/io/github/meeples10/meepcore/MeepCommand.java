a735d5493ff5e933c95b405913813371ea9ee34d
Chase <cvaughan011@gmail.com>
2018-07-14 19:13:18 -0400

    Revised MeepCommand class

--- src/io/github/meeples10/meepcore/MeepCommand.java
+++ src/io/github/meeples10/meepcore/MeepCommand.java
@@ -1,19 +1,31 @@
 package io.github.meeples10.meepcore;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public abstract class MeepCommand implements CommandExecutor {
     private String usage;
-    
+
     public MeepCommand(String usage) {
         this.usage = usage;
     }
-    
+
     public String getUsage() {
         return Messages.usage(usage);
     }
-    
+
     public String getUsageRaw() {
         return usage;
+    }
+
+    public abstract boolean run(CommandSender sender, Command cmd, String label, String[] args);
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(!run(sender, cmd, label, args)) {
+            sender.sendMessage(getUsage());
+        }
+        return true;
     }
 }
